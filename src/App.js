2faa003f116b543ed61f2691a1844ff317dea1d5
code-hoihoi <naoya2014@gmail.com>
2019-12-24 07:53:48 +0900

    Up to 56.Outputting Lists

--- src/App.js
+++ src/App.js
@@ -58,18 +58,21 @@ class App extends Component {
     let persons = null
     if (this.state.showPerson) {
       persons = (
-        <div>  
-            <Person 
-              name={this.state.persons[0].name} 
-              age={this.state.persons[0].age} />
-            <Person 
-              name={this.state.persons[1].name} 
-              age={this.state.persons[1].age} 
-              click={() => this.switchNameHandler("Hoihoi")}>My Hobbies: Racing</Person>
-            <Person 
-              name={this.state.persons[2].name} 
-              age={this.state.persons[2].age}
-            clicked={this.nameChangeHandler} />
+        <div>
+          {this.state.persons.map(person => {
+            return <Person name={person.name} age={person.age} />
+          })}
+          {/* <Person 
+            name={this.state.persons[0].name} 
+            age={this.state.persons[0].age} />
+          <Person 
+            name={this.state.persons[1].name} 
+            age={this.state.persons[1].age} 
+            click={() => this.switchNameHandler("Hoihoi")}>My Hobbies: Racing</Person>
+          <Person 
+            name={this.state.persons[2].name} 
+            age={this.state.persons[2].age}
+          clicked={this.nameChangeHandler} /> */}
         </div>
       )
     }
