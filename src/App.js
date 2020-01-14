c35c68ba932d9b54e8af744f7131a5795fb11d48
code-hoihoi <naoya2014@gmail.com>
2020-01-15 08:00:31 +0900

    :sparkles:Up to 70. Introducing Styled Components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-import Radium, { StyleRoot } from 'radium';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -109,16 +108,14 @@ class App extends Component {
     }
 
     return (
-      <StyleRoot>
-        <div className="App">
-          <h1>Hi! I am a React App.</h1>
-          <p className={classes.join(' ')}>This is really working!</p>
-          <button style={myStyle} onClick={this.toggleNameHandler}>Toggle Persons</button>
-          {persons}
-        </div>
-      </StyleRoot>
+      <div className="App">
+        <h1>Hi! I am a React App.</h1>
+        <p className={classes.join(' ')}>This is really working!</p>
+        <button style={myStyle} onClick={this.toggleNameHandler}>Toggle Persons</button>
+        {persons}
+      </div>
     );
   }
 }
 
-export default Radium(App);
+export default App;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,20 +1,28 @@
 import React from 'react';
-import Radium from 'radium';
-import './Person.css';
+import styled from 'styled-components';
+// import './Person.css';
+
+const StyledDiv = styled.div`
+    width: 60%;
+    margin: auto;
+    border: 1px solid #ccc;
+    box-shadow: 0 3px 20px rgb(3, 111, 231);
+    padding: 16px;
+    text-align: center;
+    
+    @media (min-width: 500px){
+        width: 450px;
+    }
+`;
 
 const person = ( props ) => {
-    const personStyle = {
-        '@media (min-width: 500px)': {
-            width: '450px'
-        }
-    };
     return(
-        <div className="Person" style={personStyle}>
+        <StyledDiv>
             <p onClick={props.click}>I'm {props.name}! I am { Math.floor(props.age) } years old.</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} />
-        </div>
+        </StyledDiv>
     )
 }
 
-export default Radium(person);
+export default person;
