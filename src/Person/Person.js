48a2e24560c99c330fa76494d40bb708133d1478
code-hoihoi <naoya2014@gmail.com>
2020-01-22 23:06:04 +0900

    :sparkles:Up to 74. CSS Modules & Media Queries

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import styled from 'styled-components';
-// import './Person.css';
 
-const StyledDiv = styled.div`
-    width: 60%;
-    margin: auto;
-    border: 1px solid #ccc;
-    box-shadow: 0 3px 20px rgb(3, 111, 231);
-    padding: 16px;
-    text-align: center;
-    
-    @media (min-width: 500px){
-        width: 450px;
-    }
-`;
+import classes from './Person.css';
 
 const person = ( props ) => {
     return(
-        <StyledDiv>
+        <div className={classes.Person}>
             <p onClick={props.click}>I'm {props.name}! I am { Math.floor(props.age) } years old.</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} />
-        </StyledDiv>
+        </div>
     )
 }
 
