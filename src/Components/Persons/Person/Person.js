fa4255d09f212d91b699db0c9739318e2095fffb
code-hoihoi <naoya2014@gmail.com>
2020-01-30 08:27:10 +0900

    :sparkles:Up to 91. Component Update Lifecycle (for props Changes)

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -1,30 +1,47 @@
-import React from 'react';
+import React, {Component} from 'react';
 import styled from 'styled-components';
 
 // import './Person.css';
 
-const StyledDiv = styled.div`
-    width: 60%;
-    margin: auto;
-    border: 1px solid #ccc;
-    box-shadow: 0 3px 20px rgb(3, 111, 231);
-    padding: 16px;
-    text-align: center;
-    
-    @media (min-width: 500px){
-        width: 450px;
+class Person extends Component {
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('[Person.js] shouldComponentUpdate', nextProps, nextState);
+        return true;  // need either true or false as this method deceides to update the DOM.
     }
-`;
 
-const person = ( props ) => {
-    console.log('[Person.js] rendering...');
-    return(
-        <StyledDiv>
-            <p onClick={props.click}>I'm {props.name}! I am { Math.floor(props.age) } years old.</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.change} />
-        </StyledDiv>
-    )
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('[Person.js] getSnapshotBeforeUpdate', prevProps, prevState);
+        return {message: 'snapshot msg from [Person.js]!'};
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('[Person.js] componentDidUpdate', prevProps, prevState, snapshot);
+        //return null;
+    }
+
+    render() {
+        const StyledDiv = styled.div`
+        width: 60%;
+        margin: auto;
+        border: 1px solid #ccc;
+        box-shadow: 0 3px 20px rgb(3, 111, 231);
+        padding: 16px;
+        text-align: center;
+
+        @media (min-width: 500px){
+            width: 450px;
+        }
+        `;
+
+        console.log('[Person.js] rendering...');
+        return(
+            <StyledDiv>
+                <p onClick={this.props.click}>I'm {this.props.name}! I am { Math.floor(this.props.age) } years old.</p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.change} value={this.props.name}/>
+            </StyledDiv>
+        )
+    }
 }
 
-export default person;
+export default Person;
--- src/Components/Persons/Persons.js
+++ src/Components/Persons/Persons.js
@@ -1,22 +1,39 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import Person from './Person/Person';
 
-const persons = (props) => {
-    console.log('[Persons.js] rendering...');
-    return (
-        <div>
-            {props.persons.map((person, personIndex) => {
-                return <Person 
-                    key={person.id} 
-                    name={person.name} 
-                    age={person.age}
-                    click={() => props.clicked(personIndex)}
-                    change={(event) => props.changed(event, person.id)} 
-                />
-            })}
-        </div>
-    )
+class Persons extends Component{
+    shouldComponentUpdate(nextProps, nextState) {
+        console.log('[Persons.js] shouldComponentUpdate', nextProps, nextState);
+        return true;  // need either true or false as this method deceides to update the DOM.
+    }
+
+    getSnapshotBeforeUpdate(prevProps, prevState) {
+        console.log('[Persons.js] getSnapshotBeforeUpdate', prevProps, prevState);
+        return {message: 'snapshot msg from [Persons.js]!'};
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log('[Persons.js] componentDidUpdate', prevProps, prevState, snapshot);
+        //return null;
+    }
+
+    render() {
+        console.log('[Persons.js] rendering...');
+        return (
+            <div>
+                {this.props.persons.map((person, personIndex) => {
+                    return <Person 
+                        key={person.id} 
+                        name={person.name} 
+                        age={person.age}
+                        click={() => this.props.clicked(personIndex)}
+                        change={(event) => this.props.changed(event, person.id)} 
+                    />
+                })}
+            </div>
+        )
+    }
 }
 
-export default persons;
+export default Persons;
