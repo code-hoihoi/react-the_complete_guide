bf0332abd13fee929528f630418191e086c338c3
code-hoihoi <naoya2014@gmail.com>
2020-02-12 07:57:52 +0900

    :hammer:Refactor indentation

--- src/Components/Persons/Person/Person.js
+++ src/Components/Persons/Person/Person.js
@@ -2,44 +2,44 @@ import React, {Component} from 'react';
 import styled from 'styled-components';
 
 const StyledDiv = styled.div`
-    width: 60%;
-    margin: auto;
-    border: 1px solid #ccc;
-    box-shadow: 0 3px 20px rgb(3, 111, 231);
-    padding: 16px;
-    text-align: center;
+  width: 60%;
+  margin: auto;
+  border: 1px solid #ccc;
+  box-shadow: 0 3px 20px rgb(3, 111, 231);
+  padding: 16px;
+  text-align: center;
 
-    @media (min-width: 500px){
-        width: 450px;
-    }
+  @media (min-width: 500px){
+    width: 450px;
+  }
 `;
 
 class Person extends Component {
-    shouldComponentUpdate(nextProps, nextState) {
-        console.log('[Person.js] shouldComponentUpdate', nextProps, nextState);
-        return true;  // need to return either true or false as this method deceides to update the DOM.
-    }
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[Person.js] shouldComponentUpdate', nextProps, nextState);
+    return true;  // need to return either true or false as this method deceides to update the DOM.
+  }
 
-    getSnapshotBeforeUpdate(prevProps, prevState) {
-        console.log('[Person.js] getSnapshotBeforeUpdate', prevProps, prevState);
-        return {message: 'snapshot msg from [Person.js]!'};
-    }
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('[Person.js] getSnapshotBeforeUpdate', prevProps, prevState);
+    return {message: 'snapshot msg from [Person.js]!'};
+  }
 
-    componentDidUpdate(prevProps, prevState, snapshot) {
-        console.log('[Person.js] componentDidUpdate', prevProps, prevState, snapshot);
-        //return null;
-    }
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('[Person.js] componentDidUpdate', prevProps, prevState, snapshot);
+    //return null;
+  }
 
-    render() {
-        console.log('[Person.js] rendering...');
-        return(
-            <StyledDiv>
-                <p onClick={this.props.click}>I'm {this.props.name}! I am { Math.floor(this.props.age) } years old.</p>
-                <p>{this.props.children}</p>
-                <input type="text" onChange={this.props.change} value={this.props.name}/>
-            </StyledDiv>
-        )
-    }
+  render() {
+    console.log('[Person.js] rendering...');
+    return(
+      <StyledDiv>
+        <p onClick={this.props.click}>I'm {this.props.name}! I am { Math.floor(this.props.age) } years old.</p>
+        <p>{this.props.children}</p>
+        <input type="text" onChange={this.props.change} value={this.props.name}/>
+      </StyledDiv>
+    )
+  }
 }
 
 export default Person;
