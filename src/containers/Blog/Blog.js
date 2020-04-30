3972c6e3e782d9426f8e1a4eba9f1c2e74f36782
code-hoihoi <naoya2014@gmail.com>
2020-04-30 17:27:47 +0900

    :sparkles:Up to 165. Making a Post Selectable

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,7 +7,10 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    state = {data: []};
+    state = {
+        data: [],
+        id: null
+    };
 
     componentDidMount () {
         axios.get('https://jsonplaceholder.typicode.com/posts')
@@ -16,22 +19,37 @@ class Blog extends Component {
                       return {...datum, 'author': 'Max'}; // DO NOT Forget the Spread Operator!
                   });
 
-                  this.setState({data: partial_data});
+                  this.setState({
+                    data: partial_data
+                  });
                   // console.log('[componentDidMount]', response);
             });
     }
 
+    selectedIdHandler = (selectedId) => {
+        this.setState({
+            id: selectedId
+        });
+    }
+
     render () {
         const posts = this.state.data.map((datum) => {
-            return <Post key={datum.id} title={datum.title} author={datum.author}/>
+            return (
+                <Post 
+                    key={datum.id} 
+                    title={datum.title} 
+                    author={datum.author} 
+                    clicked={() => this.selectedIdHandler(datum.id)} />
+            );
         });
+
         return (
             <div>
                 <section className="Posts">
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.id}/>
                 </section>
                 <section>
                     <NewPost />
