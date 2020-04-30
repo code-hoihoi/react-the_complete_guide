4a59ef1820640fcf782f0fab9a8edd71cdad31bc
code-hoihoi <naoya2014@gmail.com>
2020-05-01 00:38:01 +0900

    :sparkles:Up to 166. Fetching Data on Update (without Creating Infinite Loops)

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
 
 import './FullPost.css';
+import Axios from 'axios';
 
 class FullPost extends Component {
+    state = {
+        loadedPost: null
+    }
+
+    componentDidUpdate () {
+        if ((this.props.id && !this.state.loadedPost) || (this.props.id && (this.props.id !== this.state.loadedPost.id))) {
+            Axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+            .then(response => {
+                this.setState({
+                    loadedPost: response.data
+                });
+            })
+        }
+    }
+
+
     render () {
         let post = <p>Please select a Post!</p>;
-
         if (this.props.id) {
+            post =  <p>Now Loading ...</p>;
+        }
+
+        if (this.props.id && this.state.loadedPost) {
             post = (
                 <div className="FullPost">
-                    <h1>Title</h1>
-                    <p>Content</p>
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}</p>
                     <div className="Edit">
                         <button className="Delete">Delete</button>
                     </div>
